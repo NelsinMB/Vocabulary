b1a5ad93951f8af8c398b102096a24df050110dd
Nelsin Martin-Burnett <73394875+NelsinMB@users.noreply.github.com>
2025-08-03 14:43:39 +0200

    - Hopefully nothing particularly important that needs to be commented on but side effects of the structural changes performed.

--- Prototype/objects/adder.py
+++ Prototype/objects/adder.py
@@ -2,13 +2,13 @@ import sqlite3
 import requests
 import sys
 from datetime import datetime
-from Utils.db_utils import get_db_connection
+from ..utils.DBManager import get_db_connection
+from ..utils.DBManager import DBManager
 
 def add_word(word):
-    with get_db_connection() as conn:
-        cur = conn.cursor() 
-        cur.execute("CREATE TABLE IF NOT EXISTS vocab(word, definition, creation_date, last_reviewed_date, review_count)")
-        cur.execute("""INSERT INTO vocab VALUES (?, ?, ?, ?, ?) """, (word, find_definition(word), datetime.now(), -1, 0))
+    db = DBManager()
+    db.execute("CREATE TABLE IF NOT EXISTS vocab(word, definition, creation_date, last_reviewed_date, review_count)")
+    db.execute("""INSERT INTO vocab VALUES (?, ?, ?, ?, ?) """, (word, find_definition(word), datetime.now(), -1, 0))
 
 def find_definition(word):
     response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
