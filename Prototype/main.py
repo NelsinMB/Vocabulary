c4ef813cba71d6a3325e6e15e9e54000d5406819
Nelsin Martin-Burnett <73394875+NelsinMB@users.noreply.github.com>
2025-08-19 22:10:17 +0200

    Adjusts main.py to reflect other commits, including: - new user_processes directory - new commands for removing words and decks

--- Prototype/main.py
+++ Prototype/main.py
@@ -1,71 +1,13 @@
 import argparse
 import sqlite3
 from utils.DBManager import DBManager
-from objects.adder import add_word as adder_add_word
 from utils.Context import Context
-
-def add_deck(name, context):
-    db = DBManager()
-    db.execute("INSERT INTO decks (name, parent_id) VALUES (?, ?)", (name, context.current_deck_id))
-    print(f"Deck '{name}' added under deck ID {context.current_deck_id or 'root'}") # Review
-    
-def move_to_deck(name, context):
-    db = DBManager()
-    
-    if name == "parent":
-        result = db.execute(
-            "SELECT parent_id FROM decks WHERE id IS ?", 
-            (context.current_deck_id, ), 
-            fetch = True
-        )
-        parent_id = result[0][0]
-        result2 = db.execute(
-            "SELECT name FROM decks WHERE id IS ?",
-            (parent_id, ),
-            fetch = True
-        )
-        parent_name = result2[0][0]
-        context.set_deck(parent_id, parent_name)
-        print(f"Moved into deck: {parent_name}")
-    else:
-        result = db.execute(
-        "SELECT id FROM decks WHERE name = ? AND parent_id IS ?", 
-        (name, context.current_deck_id),
-        fetch = True
-        )
-        if result:
-            deck_id = result[0][0] 
-            # Result returns as a list even though name+parent is unique (need to make that it is indeed unique)
-            # Then, we want the first entry of that row since it is the id
-            context.set_deck(deck_id, name)
-            print(f"Moved into deck: {name}")
-        else:
-            print(f"Deck '{name}' not found under current deck.")
-        
-
-def add_word(word, context):
-    adder_add_word(word, context)
-
-# Lists the contents of the current deck, which consists of words and sub decks.
-def list_current_deck(context: Context):
-    db = DBManager()
-    decks = db.execute(
-        "SELECT name FROM decks WHERE parent_id IS ?", 
-        (context.current_deck_id, ),
-        fetch=True
-    )
-    decks = [entry[0] for entry in decks]
-    words = db.execute(
-        "SELECT w.term FROM words as w JOIN deck_words as dw ON w.id = dw.word_id WHERE dw.deck_id = ?",
-        (context.current_deck_id, ),
-        fetch=True
-    )
-    words = [entry[0] for entry in words]
-
-    for deck in decks:
-        print("Deck: ", deck)
-    for word in words:
-        print("Word: ",  word)
+from user_processes.add_word import add_word_procedure
+from user_processes.add_deck import add_deck
+from user_processes.move_deck import move_deck
+from user_processes.list_deck import list_deck
+from user_processes.remove_deck import remove_deck
+from user_processes.remove_word import remove_word
 
 def run_interactive():
     from utils.Context import Context
@@ -96,17 +38,29 @@ def parse_args(args_list=None):
     # ---- ADD Commands ----
     
     # We will have 'add deck' and 'add word'
-    add_parser = subparsers.add_parser("add", help="Add decks or objects")
+    add_parser = subparsers.add_parser("add", help="Add decks or words")
     add_subparsers = add_parser.add_subparsers(dest="add_command")
     
     
     add_deck = add_subparsers.add_parser("deck", help="Add a new deck")
     add_deck.add_argument("name", help="Name of the deck")
     
-    add_obj = add_subparsers.add_parser("word", help="Add a new word")
-    add_obj.add_argument("term", help="The word)")
+    add_word = add_subparsers.add_parser("word", help="Add a new word")
+    add_word.add_argument("term", help="The word")
     
-
+    
+    # ---- REMOVE Commands ----
+    
+    # We will have 'remove deck' and 'remove word'
+    remove_parser = subparsers.add_parser("remove", help="Remove decks or words")
+    remove_subparsers = remove_parser.add_subparsers(dest="remove_command")
+    
+    remove_deck = remove_subparsers.add_parser("deck", help="Remove an existing deck")
+    remove_deck.add_argument("name", help="Name of the deck")
+    
+    remove_word = remove_subparsers.add_parser("word", help="Remove a new word")
+    remove_word.add_argument("term", help="The word")
+        
     # ---- List Commands ----
     subparsers.add_parser("list")
     
@@ -127,13 +81,18 @@ def handle_args(args, context=None):
             add_deck(args.name, context)
         elif args.add_command == "word":
             print("got here")
-            add_word(args.term, context)
+            add_word_procedure(args.term, context)
         else:
             print("You must specify to add either a deck or word")
+    elif args.command == "remove":
+        if args.remove_command == "deck":
+            remove_deck(args.name, context)
+        elif args.remove_command == "word":
+            remove_word(args.term, context)
     elif args.command == "list":
-        list_current_deck(context)
+        list_deck(context)
     elif args.command == "move":
-        move_to_deck(args.name, context)
+        move_deck(args.name, context)
     else:
         print("Unknown command.")
     
